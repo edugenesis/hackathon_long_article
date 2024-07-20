80030df34f312361a220cdd6491b4ff0321ee5c8
marchellodev <marchellodev@gmail.com>
2024-07-20 14:57:06 +0300

    hopefully fix showing ads (loading them in adv.tsx)

--- src/components/adv.tsx
+++ src/components/adv.tsx
@@ -1,3 +1,25 @@
-﻿export function Advert(props: { id: string }) {
+﻿import { onMount } from "solid-js";
+
+export function Advert(props: { id: string }) {
+    onMount(() => {
+        console.log('loading ad', props.id)
+
+        window.pbjs.que.push(function () {
+            window.pbjs.requestBids({
+                timeout: 1000,
+                adUnitCodes: [props.id],
+                bidsBackHandler: function () {
+                    window.pbjs.setTargetingForGPTAsync([props.id]);
+
+                    const target = window.googletag
+                        .pubads()
+                        .getSlots()
+                        .find((slot) => slot.getSlotElementId() === props.id);
+
+                    target && window.googletag.pubads().refresh([target]);
+                }
+            });
+        });
+    });
     return <div id={props.id} style="min-height: 280px" data-slot-type="1"></div>;
 }
