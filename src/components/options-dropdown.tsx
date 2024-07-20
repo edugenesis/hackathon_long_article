9955aea98177d35b014c3f8b60a892920ea1a596
Vlad Prudius <vladvlpr006@gmail.com>
2024-07-20 21:27:28 +0300

    feat: add options-dropdown component

---
+++ src/components/options-dropdown.tsx
@@ -0,0 +1,66 @@
+﻿import { createSignal } from "solid-js";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+    DropdownMenuSeparator,
+    DropdownMenuRadioGroup,
+    DropdownMenuRadioItem,
+} from "~/components/ui/dropdown-menu";
+
+export interface Options {
+    ads: "disabled" | "enabled" | "optimized";
+    embeds: "disabled" | "enabled" | "optimized";
+}
+
+const loadOptions = (): Options => {
+    const storedOptions = localStorage.getItem("options");
+    if (storedOptions) {
+        return JSON.parse(storedOptions);
+    }
+    return {
+        ads: "enabled",
+        embeds: "enabled"
+    };
+};
+
+export const [options, setOptions] = createSignal<Options>(loadOptions());
+
+
+export function OptionsDropdown() {
+    const updateOptions = (key: keyof Options, value: Options[keyof Options]) => {
+        setOptions((prev) => {
+            const newOptions = { ...prev, [key]: value };
+            localStorage.setItem("options", JSON.stringify(newOptions));
+            return newOptions;
+        });
+    };
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger as="button">Options</DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuItem disabled class="font-bold">
+                    Ads
+                </DropdownMenuItem>
+                <DropdownMenuRadioGroup value={options().ads} onChange={(value) => updateOptions("ads", value as Options["ads"])}>
+                    <DropdownMenuRadioItem value="disabled">Disabled</DropdownMenuRadioItem>
+                    <DropdownMenuRadioItem value="enabled">Enabled</DropdownMenuRadioItem>
+                    <DropdownMenuRadioItem value="optimized">Optimized</DropdownMenuRadioItem>
+                </DropdownMenuRadioGroup>
+
+                <DropdownMenuSeparator />
+
+                <DropdownMenuItem disabled class="font-bold">
+                    Embeddings
+                </DropdownMenuItem>
+                <DropdownMenuRadioGroup value={options().embeds} onChange={(value) => updateOptions("embeds", value as Options["embeds"])}>
+                    <DropdownMenuRadioItem value="disabled">Disabled</DropdownMenuRadioItem>
+                    <DropdownMenuRadioItem value="enabled">Enabled</DropdownMenuRadioItem>
+                    <DropdownMenuRadioItem value="optimized">Optimized</DropdownMenuRadioItem>
+                </DropdownMenuRadioGroup>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+}
