293919ed0239770792b896986feb2900768ee7a7
marchellodev <marchellodev@gmail.com>
2024-07-20 16:41:59 +0300

    let's try loading ads with 50% chance

--- src/components/adv.tsx
+++ src/components/adv.tsx
@@ -3,13 +3,13 @@ import { runAdUnit } from './ad_helpers';
 
 export function Advert(props: { id: string }) {
   onMount(async () => {
-    if (!['in_article_1', 'in_article_4'].includes(props.id)) return;
+    if (Math.random() > 0.5) return;
     await new Promise((r) => setTimeout(r, 2000));
     runAdUnit(props.id);
     console.log('loading ad', props.id);
   });
 
   return (
-    <div id={props.id} class="p-4 border border-gray-200 rounded-lg" style="min-height: 280px" data-slot-type="1"></div>
+    <div id={props.id} class="p-4 border border-gray-200 rounded-lg" style="min-height: 280px"></div>
   );
 }
