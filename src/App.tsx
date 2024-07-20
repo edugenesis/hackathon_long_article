f71f0f062fbf3e9b47ad34ef5386c9006497d53d
kujo205 <kuc8301@gmail.com>
2024-07-20 15:40:11 +0300

    feat: add automatic scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import type { Component } from 'solid-js';
+import { Component, createEffect, createSignal } from 'solid-js';
 
-import  './app.css';
+import './app.css';
 import { ContentBlocks } from './services/content_service';
 
+const scrollOffsetPX = 15;
+const scrollFunctionTriggerInterval = 350;
+
 const App: Component = () => {
+  function scrollHandler() {
+    const newOffset = window.scrollY + scrollOffsetPX;
+    scrollTo({
+      top: newOffset
+    });
+  }
+
+  createEffect(() => {
+    const interval = setInterval(() => {
+      requestAnimationFrame(() => {});
+      scrollHandler();
+    }, scrollFunctionTriggerInterval);
+    return () => clearInterval(interval);
+  });
+
   return (
     <div>
       <ContentBlocks />
