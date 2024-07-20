32d96ba605be27c8d47da120ec81bedc1833b147
marchellodev <marchellodev@gmail.com>
2024-07-20 14:39:38 +0300

    upd adv (add other props)

--- src/components/adv.tsx
+++ src/components/adv.tsx
@@ -1,5 +1,3 @@
-﻿
-
-export function Advert(props: { id: string }) {
-    return <div id={props.id}></div>;
+﻿export function Advert(props: { id: string }) {
+    return <div id={props.id} style="min-height: 280px" data-slot-type="1"></div>;
 }
