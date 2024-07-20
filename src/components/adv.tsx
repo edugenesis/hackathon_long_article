3b6798be29031c81121766fbe14a582e2704051e
marchellodev <marchellodev@gmail.com>
2024-07-20 16:52:12 +0300

    let's try: enable ads only for first 3 blocks

--- src/components/adv.tsx
+++ src/components/adv.tsx
@@ -3,13 +3,12 @@ import { runAdUnit } from './ad_helpers';
 
 export function Advert(props: { id: string }) {
   onMount(async () => {
+    if (!['in_article_1', 'in_article_2', 'in_article_3'].includes(props.id)) return;
     // if (Math.random() > 0.1) return;
     await new Promise((r) => setTimeout(r, 2000));
     runAdUnit(props.id);
     console.log('loading ad', props.id);
   });
 
-  return (
-    <div id={props.id} class="p-4 border border-gray-200 rounded-lg" style="min-height: 280px"></div>
-  );
+  return <div id={props.id} class="p-4 border border-gray-200 rounded-lg" style="min-height: 280px"></div>;
 }
