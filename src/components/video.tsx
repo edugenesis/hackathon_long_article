a0e670a03eda3a7d373d8fba710ea82865b338bc
marchellodev <marchellodev@gmail.com>
2024-07-20 19:21:26 +0300

    let's try removing video

--- src/components/video.tsx
+++ src/components/video.tsx
@@ -6,11 +6,11 @@ export function Video(props: { id: string }) {
     onMount(() => {
         if (!div) throw Error('video div not found');
 
-        const vidazoo = document.createElement('script');
-        vidazoo.async = true;
-        vidazoo.src = 'https://static.vidazoo.com/basev/vwpt.js';
-        vidazoo.setAttribute('data-widget-id', '5f7c82bd819a8b00049dd9d6');
-        div.appendChild(vidazoo);
+        // const vidazoo = document.createElement('script');
+        // vidazoo.async = true;
+        // vidazoo.src = 'https://static.vidazoo.com/basev/vwpt.js';
+        // vidazoo.setAttribute('data-widget-id', '5f7c82bd819a8b00049dd9d6');
+        // div.appendChild(vidazoo);
     });
 
     return <div id={props.id} ref={div}></div>;
