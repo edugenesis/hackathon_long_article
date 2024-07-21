effaf220dd7fd12ea5ccb6258854b842c53bef20
marchellodev <marchellodev@gmail.com>
2024-07-21 09:31:04 +0300

    fix img's lazy offloading (removing img from dom completely)

--- src/components/content-items/image.tsx
+++ src/components/content-items/image.tsx
@@ -1,29 +1,40 @@
-﻿import { LazyOffloading } from '../LazyOffloading';
+﻿import { createSignal, Show } from 'solid-js';
+import { LazyOffloading } from '../LazyOffloading';
 
 export let isBannerSet: boolean = false;
 
-export function Image(props: { src: string; optimize: boolean, isBanner: boolean }) {
-  let img: HTMLImageElement | undefined;
+export function Image(props: { src: string; optimize: boolean; isBanner: boolean }) {
+  let wrapper: HTMLImageElement | undefined;
+
+  const [showImg, setShowImg] = createSignal(false);
 
   function init(isInitedOnce: boolean) {
-    if (!img) return;
-    img.setAttribute('src', props.src);
+    setShowImg(true);
   }
 
   function destroy() {
-    if (!img) return;
+    if (!wrapper) return;
 
-    const height = img!.offsetHeight;
-    if (height != 0) img.style.height = `${height}px`;
+    const height = wrapper!.offsetHeight;
+    if (height != 0) wrapper.style.height = `${height}px`;
 
-    img.removeAttribute('src');
+    setShowImg(false);
   }
-  
+
   if (props.isBanner && !isBannerSet) isBannerSet = true;
-  
+
   return (
     <LazyOffloading init={init} destroy={destroy} optimize={props.optimize}>
-      <img src={props.src} alt="" class={`h-auto rounded-lg ${props.isBanner ? 'w-full h-80 object-cover' : 'w-[75%]'} mx-auto`} loading="lazy" ref={img}/>
+      <div ref={wrapper}>
+        <Show when={showImg()}>
+          <img
+            src={props.src}
+            alt=""
+            class={`h-auto rounded-lg ${props.isBanner ? 'w-full h-80 object-cover' : 'w-[75%]'} mx-auto`}
+            loading="lazy"
+          />
+        </Show>
+      </div>
     </LazyOffloading>
   );
 }
