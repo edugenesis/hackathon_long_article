17327bf2091d095bb099d4a07157e8425f56fe56
kujo205 <kuc8301@gmail.com>
2024-07-21 08:40:06 +0300

    feat: make images lazy

--- src/components/content-items/image.tsx
+++ src/components/content-items/image.tsx
@@ -1,5 +1,3 @@
-﻿
-
-export function Image(props: { src: string }) {
-  return <img src={props.src} alt="" class="h-auto rounded-lg" />;
+﻿export function Image(props: { src: string }) {
+  return <img src={props.src} alt="" class="h-auto rounded-lg" loading="lazy" />;
 }
